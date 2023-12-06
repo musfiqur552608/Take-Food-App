d3e9df43e4666a5f86b981b51a8431835e59af55
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-12-07 00:24:09 +0600

    navigation done

---
+++ app/src/main/java/org/freedu/takefood/MainActivity.kt
@@ -0,0 +1,20 @@
+package org.freedu.takefood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController = Navigation.findNavController(this, R.id.fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
+    }
+}
